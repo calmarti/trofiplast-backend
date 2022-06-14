bf260a9a7e760b789eaae73780c6543403bef996
Ivan Martinez <ivmartinezc@gmail.com>
2022-06-14 13:09:52 +0200

    inicializacion de la base de datos con muestra de items

--- initDb.js
+++ initDb.js
@@ -1,22 +1,45 @@
 "use strict";
 require("dotenv").config();
-require("./lib/connectMongoose");
+const conn = require("./lib/connectMongoose");
+const fs = require("fs");
 
 //TODO: inicialización falla al intentar hacer el cast de date1 (por ser String y no Date), ¿meter un setter en el Schema Type y en el excel separar en campo año y campo mes?
 
-const sample = require("./sample.json"); //sample needs to be an array for 'insertMany' to work
+const sample = "./sample.json";
 const Item = require("./models/Item");
 
-const initialize = async function () {
+const loadJson = async function (file) {
+  const dataPromise = new Promise((resolve, reject) => {
+    fs.readFile(file, function (error, data) {
+      resolve(data);
+      reject(error);
+    });
+  });
   try {
-    const result = await Item.deleteMany({});
-    console.log(`Deleted items: ${result.deletedCount}`);
-    const newItems = await Item.insertMany(sample.items);
-    console.log(`New inserted items: ${newItems.length}`);
-    newItems.save();
+    const rawData = await dataPromise;
+    console.log(`Initialization file ${file} loaded`);
+    if (!rawData) {
+      console.log(`${file} is empty!`);
+      return;
+    }
+    const data = JSON.parse(rawData).items;
+    console.log(`Initialization file ${file} parsed`);
+    return data;
   } catch (error) {
     console.log(error);
   }
 };
 
-initialize();
+const initialize = async function () {
+  try {
+    const result = await Item.deleteMany({});
+    console.log(`Deleted items: ${result.deletedCount}`);
+    const data = await loadJson(sample);
+    const newItems = await Item.insertMany(data);
+    console.log(`New inserted items: ${newItems.length}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initialize().then(() => conn.close());
--- models/Item.js
+++ models/Item.js
@@ -1,18 +1,40 @@
 const mongoose = require("mongoose");
 
+//TODO: estudiar tema validaciones automáticas del Modelo, validaciones con mongoose y validaciones con express-validator
+//Setters allow you to transform the data before it gets to the raw mongodb document or query.
+//TODO: setter: antes de ser persistidos los campos 'Date' deben ser pasar de string de tipo "DD/MM/YYYY" a un objeto Date así: new Date(YYYY, MM)
+
+function changeDateFormat(value) {
+  if (value instanceof Date) {
+    return;
+  } else {
+    const splittedValue = value.split("/", 3);
+    if (splittedValue.length !== 1) {
+      const formattedValue = new Date(splittedValue[2], splittedValue[1]);
+    } else {
+      const formattedValue = new Date(splittedValue[0]);
+    }
+  }
+}
+
+
 const itemSchema = mongoose.Schema({
-  group /*taxonomic group*/ : { type: String },
-  family: { type: String },
-  genus: { type: String },
-  species: { type: String },
-  location: { type: String },
-  country: { type: String },
-  date1: { type: Date, /*get: changeDateFormat*/ },    //cambiar formato del TYPE de ser necesario con plugin "mongoose-plugin-date-format" o bien cambiar el formato al recuperar de la bd con un custom getter (changeDateFormat)
-  //date 2
-  //date 3
-  items: { type: String }, 
-  reference: { type: String }, 
-  href: {type: String }, //type preeliminar, tal vez será un custom type
+  taxonomic_group /*taxonomic group*/: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  family: { type: String, required: true, index: true },
+  genus: { type: String, required: true, index: true },
+  species: { type: String, required: true, index: true },
+  area: { type: String, required: true },
+  country: { type: String, required: true, index: true },
+  date1: { type: Date, set: changeDateFormat }, //cambiar formato del TYPE de ser necesario con plugin "mongoose-plugin-date-format" o bien cambiar el formato al recuperar de la bd con un custom getter (changeDateFormat)
+  date2: { type: Date, set: changeDateFormat },
+  date3: { type: Date, set: changeDateFormat },
+  summary: { type: String },
+  reference: { type: String },
+  href: { type: String }, //type preeliminar, tal vez será un custom type
   image: { type: String },
 });
 
