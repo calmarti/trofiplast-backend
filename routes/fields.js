f2641fe080ad1cebf392f3140d81b2c6558c11f9
Ivan Martinez <ivmartinezc@gmail.com>
2022-06-26 19:12:40 +0200

    fix: filter en getFieldValues del endpoint fields

--- routes/fields.js
+++ routes/fields.js
@@ -5,14 +5,13 @@ const Item = require("../models/Item");
 //rutas que devuelven array de valores posibles de los distintos campos
 router.get("/:field", async (req, res, next) => {
   const field = req.params.field;
-  //   console.log(field);
   try {
-    let values = await Item.getFieldValues(field);
-    console.log("antes de filter", values);
-    const newValues = values.filter((elem) => (elem !== "N/A" && elem!=="N.A." && elem!=="NA"));
+    const values = await Item.getFieldValues(field);
+    // console.log("antes de filter", values);
+    const result = values.filter((elem) => (elem !== "N/A" && elem!=="N.A." && elem!=="NA"));
 
-    console.log("despues de filter", newValues);
-    res.json({ result: newValues });
+    // console.log("despues de filter", result);
+    res.json({ result });
   } catch (err) {
     next(err);
   }
